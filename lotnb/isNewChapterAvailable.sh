7f7e0b9c51cf30c17a03278905544b2104553611
VarunKS <ksvarun001@gmail.com>
2023-03-19 12:21:37 +0530

    first commit

---
+++ lotnb/isNewChapterAvailable.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+lastReadChapter=$(head -n 1 readChapters.txt)
+newChapterToRead=`expr $lastReadChapter + 1`
+link=https://legendofnorthernblade.com/manga/legend-of-the-northern-blade-chapter-$newChapterToRead
+res=$(curl $link 2> /dev/null)
+
+if [ ${#res} != 0 ]
+then
+    echo 'New Chapter is available click -' $link
+fi
