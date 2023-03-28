05b4a6bbeea6efc9dddb2dacff14eb4aa698286c
VarunKS <ksvarun001@gmail.com>
2023-03-29 00:29:58 +0530

    updated readChapters patch to absolute path

--- lotnb/isNewChapterAvailable.sh
+++ lotnb/isNewChapterAvailable.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-lastReadChapter=$(head -n 1 readChapters.txt)
+lastReadChapter=$(head -n 1 ~/Developer/shell/lotnb/readChapters.txt)
 newChapterToRead=`expr $lastReadChapter + 1`
 link=https://legendofnorthernblade.com/manga/legend-of-the-northern-blade-chapter-$newChapterToRead
 res=$(curl $link 2> /dev/null)
